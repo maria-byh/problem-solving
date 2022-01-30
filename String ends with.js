d85593076be038130e96eba242341cca37d621fa
maria2001-byh <59501786+maria2001-byh@users.noreply.github.com>
2022-01-30 13:50:46 +0100

    commit

--- String ends with.js
+++ String ends with.js
@@ -1,6 +1,8 @@
 /**** 
 
 **Problem 
+source:codewars. 
+    https://www.codewars.com/kata/string-ends-with/train/javascript
  
 Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
